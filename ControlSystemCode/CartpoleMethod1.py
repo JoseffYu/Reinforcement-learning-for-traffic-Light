26e9ecfd76239012adfed165ef0bff2756e414be
Yanlin <yuyanlin1234@163.com>
2024-01-06 14:39:59 +0000

    new file:   Cartpole_method1_reward_epi.png 	new file:   Cartpole_method2_reward_epi.png 	new file:   ControlSystemCode/CartpoleMethod1.py 	new file:   ControlSystemCode/__pycache__/CarpoleMethod1.cpython-39.pyc 	new file:   ControlSystemCode/__pycache__/CartpoleMethod1.cpython-39.pyc 	new file:   ControlSystemCode/__pycache__/cartpoleMethod2.cpython-39.pyc 	new file:   ControlSystemCode/__pycache__/test.cpython-39.pyc 	new file:   ControlSystemCode/cartpoleMethod2.py 	modified:   ControlSystemCode/mainCartpole.py 	new file:   ControlSystemCode/pytorchCartpole.py 	new file:   Hierarchical RL_HIDIO.pdf

---
+++ ControlSystemCode/CartpoleMethod1.py
@@ -0,0 +1,164 @@
+
+import matplotlib
+import random
+import math
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+from collections import namedtuple, deque
+
+device ="cpu"
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+is_ipython = 'inline' in matplotlib.get_backend()
+if is_ipython:
+    from IPython import display
+
+plt.ion()
+
+
+class network(nn.Module):
+
+    def __init__(self, n_observations, n_actions):
+        super(network, self).__init__()
+        self.layer1 = nn.Linear(n_observations, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, n_actions)
+
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
+
+
+class ReplayBuffer(object):
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+        self.memory_len = 0
+
+    def push(self, *args):
+        self.memory.append(Transition(*args))
+        self.memory_len += 1
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def memoryLen(self):
+        return self.memory_len
+
+   
+class DQN:
+    def __init__(self,
+                 env,
+                 mode,
+                 input_dim,
+                 output_dim,
+                 gamma,
+                 replay_size,
+                 batch_size,
+                 eps_start: float,
+                 eps_end: float,
+                 eps_decay: int,
+                 TAU,
+                 LR):
+        self.mode = mode
+        self.n_actions = output_dim  #size of action space
+        self.gamma = gamma
+        self.TAU = TAU
+        self.batch_size = batch_size
+        self.eps_start = eps_start
+        self.eps_end = eps_end
+        self.eps_decay = eps_decay
+        self.lr = LR
+        self.episode_durations = []
+        self.memory = None
+        self.env = env
+
+        self.policy_net = network(input_dim, output_dim).to(device)
+        self.target_net = network(input_dim, output_dim).to(device)
+        self.target_net_state_dict = self.target_net.state_dict()
+        self.policy_net_state_dict = self.policy_net.state_dict()
+        self.replay_size = replay_size
+
+        self.optimizer = torch.optim.Adam(self.policy_net.parameters(), lr=self.lr, amsgrad=True)
+        self.loss_func = nn.MSELoss()
+        self.losses = []
+        self.expected_values = []
+        
+        self.learn_step_counter = 0  # for target updating
+
+
+    def selectAction(self, state, steps_done):
+        #original_state = state
+        sample = random.random()
+        eps_threshold = self.eps_end + (self.eps_start - self.eps_end) * math.exp(-1. * steps_done / self.eps_decay)
+        self.learn_step_counter += 1
+        if sample > eps_threshold:
+            with torch.no_grad():
+                return self.policy_net(state).max(1).indices.view(1, 1)
+        else:
+            return torch.tensor([self.env.action_space.sample()], device=device, dtype=torch.long).unsqueeze(0)
+
+
+    def plot_durations(self,show_result=False):
+        plt.figure(1)
+        durations_t = torch.tensor(self.episode_durations, dtype=torch.float)
+        if show_result:
+            plt.title('Result')
+        else:
+            plt.clf()
+            plt.title('Training...')
+        plt.xlabel('Episode')
+        plt.ylabel('Duration')
+        plt.plot(durations_t.numpy())
+        # Take 100 episode averages and plot them too
+        if len(durations_t) >= 100:
+            means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+            means = torch.cat((torch.zeros(99), means))
+            plt.plot(means.numpy())
+
+        plt.pause(0.001)  # pause a bit so that plots are updated
+        if is_ipython:
+            if not show_result:
+                display.display(plt.gcf())
+                display.clear_output(wait=True)
+            else:
+                display.display(plt.gcf())
+    
+    
+                
+    def learn(self):
+        if self.learn_step_counter < self.batch_size:
+            return
+
+        transitions = self.memory.sample(self.batch_size)
+        batch = Transition(*zip(*transitions))
+        
+        state_batch = torch.cat(batch.state)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
+        
+        next_state_batch = torch.cat(batch.next_state)
+        state_action_values = self.policy_net(state_batch).gather(1, action_batch)
+        
+        with torch.no_grad():
+            argmax_action = self.target_net(next_state_batch).max(1)[1].view(1,self.batch_size)
+        expected_state_action_values = reward_batch + self.gamma * self.target_net(next_state_batch).gather(1, argmax_action)  # Compute the expected Q values
+
+        # Compute Huber loss
+        loss = self.loss_func(state_action_values, expected_state_action_values)
+        
+        #store expected values and loss of each step
+        self.losses.append(loss.item())
+        self.expected_values.extend(expected_state_action_values.detach().numpy())
+        
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        torch.nn.utils.clip_grad_value_(self.policy_net.parameters(), 100)
+        self.optimizer.step()
+        
--- ControlSystemCode/mainCartpole.py
+++ ControlSystemCode/mainCartpole.py
@@ -20,18 +20,20 @@ def main():
     state_size = env.observation_space.shape[0]
     action_size = env.action_space.n
     
-    agent = DQN(env = env,mode="train",input_dim=state_size,output_dim=action_size,gamma=0.95,replay_size=20000,batch_size=32,eps_start=0.95,eps_end=0.05,eps_decay=30000,TAU=0.005,LR=1e-4)
+    agent = DQN(env = env,mode="train",input_dim=state_size,output_dim=action_size,gamma=0.95,replay_size=20000,batch_size=128,eps_start=0.95,eps_end=0.05,eps_decay=1000,TAU=0.005,LR=1e-4)
     replay_buffer = ReplayBuffer(agent.replay_size)
     agent.memory = replay_buffer
     
-    for episode in range(1000):
+    for episode in range(500):
         state, info = env.reset()
         state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
+        total_reward = 0
 
         for t in count():
             env.render()
             action = agent.selectAction(state,agent.learn_step_counter)
             observation, reward, done, _ = env.step(action.item())[:4]
+            total_reward += reward
             reward = torch.tensor([reward], device=device)
 
             next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
@@ -39,14 +41,16 @@ def main():
             state = next_state
             
             agent.learn()
-
+            
+            agent.target_net_state_dict = agent.target_net.state_dict()
+            agent.policy_net_state_dict = agent.policy_net.state_dict()
             for param in agent.policy_net_state_dict:
                 agent.target_net_state_dict[param] = agent.policy_net_state_dict[param]*agent.TAU + agent.policy_net_state_dict[param]*(1-agent.TAU)
 
             agent.target_net.load_state_dict(agent.target_net_state_dict)
             
             if done:
-                agent.episode_durations.append(t+1)
+                agent.episode_durations.append(total_reward)
                 agent.plot_durations()
                 break
         print('i_episode:', episode)
@@ -76,7 +80,7 @@ def main():
     plt.title("Expected Values Over Time")
     plt.xlabel("learn_steps")
     plt.ylabel("Expected Value")
-    plt.savefig('/Users/yuyanlin/Desktop/AdaptiveTrafficLight/Cartpole_method1_reward_epi.png')
+    plt.savefig('/Users/yuyanlin/Desktop/AdaptiveTrafficLight/Cartpole_method2_reward_epi.png')
 
 plt.show()
 
